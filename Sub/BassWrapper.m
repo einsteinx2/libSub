17408b10a776d3a1d948cc0ee19f225b3bdfeba9
Benjamin Baron <ben@einsteinx2.com>
2013-03-30 18:30:54 -0700

    - Disabled MPC plugin due to crashes

--- Sub/BassWrapper.m
+++ Sub/BassWrapper.m
@@ -41,13 +41,13 @@ static NSUInteger _bassOutputBufferLengthMillis = 0;
         BASS_PluginLoad(&BASSFLACplugin, 0); // load the Flac plugin
         BASS_PluginLoad(&BASSWVplugin, 0); // load the WavePack plugin
         BASS_PluginLoad(&BASS_APEplugin, 0); // load the Monkey's Audio plugin
-        BASS_PluginLoad(&BASS_MPCplugin, 0); // load the MusePack plugin
+        //BASS_PluginLoad(&BASS_MPCplugin, 0); // load the MusePack plugin
         BASS_PluginLoad(&BASSOPUSplugin, 0); // load the OPUS plugin
 #else
         BASS_PluginLoad("libbassflac.dylib", 0); // load the Flac plugin
         BASS_PluginLoad("libbasswv.dylib", 0); // load the WavePack plugin
         BASS_PluginLoad("libbass_ape.dylib", 0); // load the Monkey's Audio plugin
-        BASS_PluginLoad("libbass_mpc.dylib", 0); // load the MusePack plugin
+        //BASS_PluginLoad("libbass_mpc.dylib", 0); // load the MusePack plugin
         BASS_PluginLoad("libbassopus.dylib", 0); // load the OPUS plugin
 #endif
 	}
