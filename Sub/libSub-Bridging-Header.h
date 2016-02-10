205216f6b763ca10095f1ee45a2ab00c74e809e7
Ben Baron <ben@einsteinx2.com>
2016-02-10 14:18:44 -0800

    Integrated EX2Kit, CocoaLumberjack, CocoaHTTPServer, and CocoaAsyncSocket dependencies. No more submodules!

---
+++ Sub/libSub-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  libSub-Bridging-Header.h
+//  libSub
+//
+//  Created by Benjamin Baron on 2/10/16.
+//  Copyright © 2016 Einstein Times Two Software. All rights reserved.
+//
+
+#import "libSubImports.h"
